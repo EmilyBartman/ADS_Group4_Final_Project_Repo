cb8d2d1a8122faae0bf08eafe05048f8e0051799
alrisc <115005019+alrisc@users.noreply.github.com>
2023-10-20 19:19:44 -0400

    Another test

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+
+print(os.listdir(os.curdir))
 
 print("Hello world!")
 
 df = pd.read_csv('imdb_top_1000.csv')
 
+#test
