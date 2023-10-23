63dc92af5a57e598db53b440a051108ed62f5652
aschmig <aschmig@iu.edu>
2023-10-23 13:57:58 -0400

    Data Cleaning Commit

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import pandas as pd
 import numpy as np
 import os
 
-print(os.listdir(os.curdir))
-
-print("Hello world!")
-
 df = pd.read_csv('imdb_top_1000.csv')
+print(df.shape)
 
-print(df)
-#test
-#testidytest
+df['Released_Year'] = df['Released_Year'].apply(lambda x: 1995 if x == 'PG' else int(x))
+
+df = df.dropna()
+print(df.shape)
+
+df['Gross'] = df['Gross'].apply(lambda x: x.replace(',','') if ',' in x else x).astype(int)
+df['Runtime'] = df['Runtime'].apply(lambda x: x.replace(' min','') if ' min' in x else x).astype(int)
+
+print(df.dtypes)
