4cf38f7a0fd3bc166587e04e4b392f7976441757
EmilyBartman <90109327+EmilyBartman@users.noreply.github.com>
2023-12-03 17:15:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 from sklearn.linear_model import LinearRegression
 import pandas as pd
